71da9f70a3f81d5fe1d063eaf2bb2ba67849f2cb
Hanif Abyan A <57577618+Ayyasythz@users.noreply.github.com>
2023-12-31 16:01:42 +0700

    improve organization

--- resources/views/organizations/index.blade.php
+++ resources/views/organizations/index.blade.php
@@ -22,14 +22,14 @@
   </svg>
 
   @if ($message = Session::get('success'))
-  <div class="alert alert-success d-flex align-items-center" role="alert">
+  <div class="alert alert-success d-flex align-items-center" role="alert" id="alert">
     <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
     <div>{{ $message }}</div>
   </div><br>
   @endif
 
   @if($message = Session::get('error'))
-  <div class="alert alert-danger d-flex align-items-center" role="alert">
+  <div class="alert alert-danger d-flex align-items-center" role="alert" id="alert">
     <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
     <div>{{ $message }}</div>
   </div><br>
@@ -47,7 +47,7 @@
           <th scope="col">Action</th>
         </tr>
       </thead>
-      
+
       @foreach ($organizations as $organization)
         <tr>
           <th scope="row">{{ ++$i }}</th>
@@ -68,9 +68,11 @@
       @endforeach
     </table>
   </div>
-
-<div clas="d-flex justify-content-start">
-    { $organizations->links() }
+<div class="d-flex justify-content-start">
+    {{$organizations->links()}}
 </div>
 
 @endsection
+@push('script')
+
+@endpush
