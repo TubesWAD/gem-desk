109a678a1209428aa4327022d090cb54b9b5d228
Hanif Abyan A <57577618+Ayyasythz@users.noreply.github.com>
2024-01-02 16:07:59 +0700

    forget redirect login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -48,7 +48,7 @@ class AuthController extends Controller
         $credentials = $request->only('email', 'password');
 
         if (Auth::attempt($credentials)) {
-            return redirect()->intended('home');
+            return redirect()->intended('tickets');
         }
 
         return redirect()->back()->with('error', 'Invalid Credentials');
